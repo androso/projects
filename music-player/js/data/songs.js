77ec10f49e3a592157665f0d6cbb5e8fe80c60c1
Anibal <androso421@gmail.com>
2021-08-21 09:33:03 -0600

    starting files for new project, a music-player

---
+++ music-player/js/data/songs.js
@@ -0,0 +1,92 @@
+export const songsList = [{
+    "id": "1",
+    "url": "../../music/01. I CAN'T STOP ME.mp3",
+    "time": "03:25",
+    "title": "I CAN'T STOP ME",
+    "artist": "TWICE"
+  },
+  {
+    "id": "2",
+    "url": "../../music/02. HELL IN HEAVEN.mp3",
+    "time": "02:59",
+    "title": "HELL IN HEAVEN",
+    "artist": "TWICE"
+  },
+  {
+    "id": "3",
+    "url": "../../music/03. UP NO MORE.mp3",
+    "time": "03:34",
+    "title": "UP NO MORE",
+    "artist": "TWICE"
+  },
+  {
+    "id": "4",
+    "url": "../../music/04. DO WHAT WE LIKE.mp3",
+    "time": "02:59",
+    "title": "DO WHAT WE LIKE",
+    "artist": "TWICE"
+  },
+  {
+    "id": "5",
+    "url": "../../music/05. BRING IT BACK.mp3",
+    "time": "03:28",
+    "title": "BRING IT BACK",
+    "artist": "TWICE"
+  },
+  {
+    "id": "6",
+    "url": "../../music/06. BELIEVER.mp3",
+    "time": "03:16",
+    "title": "BELIEVER",
+    "artist": "TWICE"
+  },
+  {
+    "id": "7",
+    "url": "../../music/07. QUEEN.mp3",
+    "time": "03:13",
+    "title": "QUEEN",
+    "artist": "TWICE"
+  },
+  {
+    "id": "8",
+    "url": "../../music/08. GO HARD.mp3",
+    "time": "03:01",
+    "title": "GO HARD",
+    "artist": "TWICE"
+  },
+  {
+    "id": "9",
+    "url": "../../music/09. SHOT CLOCK.mp3",
+    "time": "03:29",
+    "title": "SHOT CLOCK",
+    "artist": "TWICE"
+  },
+  {
+    "id": "10",
+    "url": "../../music/10. HANDLE IT.mp3",
+    "time": "02:51",
+    "title": "HANDLE IT",
+    "artist": "TWICE"
+  },
+  {
+    "id": "11",
+    "url": "../../music/11. DEPEND ON YOU.mp3",
+    "time": "03:18",
+    "title": "DEPEND ON YOU",
+    "artist": "TWICE"
+  },
+  {
+    "id": "12",
+    "url": "../../music/12. SAY SOMETHING.mp3",
+    "time": "04:07",
+    "title": "SAY SOMETHING",
+    "artist": "TWICE"
+  },
+  {
+    "id": "13",
+    "url": "../../music/13. BEHIND THE MASK.mp3",
+    "time": "03:38",
+    "title": "BEHIND THE MASK",
+    "artist": "TWICE"
+  }
+];
