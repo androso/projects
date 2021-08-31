732d298da86099e7fff5808b8e8d4d17f3256c01
Anibal <androso421@gmail.com>
2021-08-31 10:34:26 -0600

    rendering game screen

--- hangman-v1/js/modules/game.js
+++ hangman-v1/js/modules/game.js
@@ -1,6 +1,36 @@
+import Home from "./home.js";
+import {sound as sounds} from "../data/sound.js"
+
+// We're gonna need an array of words
+//
 const Game = (() => {
+    const $hangmanContainer = document.querySelector(".hangman__container");
     const init = () => {
-        console.log("New Game");
+        render();
+        addListeners();
+    }
+    const render = () => {
+        let markup = `
+        <p class="hangman__stats">Lives: 7</p>
+        <h1 class="hangman__title">Hangman</h1>
+        <canvas class="hangman__board">
+        </canvas>
+        <div class="hangman__word">
+        </div>
+        <p class="hangman__instructions">Pick an alphabet below to guess the whole word.</p>
+        <ul class="hangman__letters">
+            <li class="hangman__letter">a</li>
+        </ul>
+        <button class="button hangman__trigger">Main Menu</button>
+        `
+        $hangmanContainer.innerHTML = markup;
+    }
+    const addListeners = () => {
+        const $mainMenuButton = document.querySelector(".hangman__trigger");
+        $mainMenuButton.addEventListener("click", () => {
+            Home.init();
+            sounds.click.play();
+        });
     }
     return {
         init,
